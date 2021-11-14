4efc7052f1c9e15b3197b33cf06bcee2febd5600
Enrique Sanchez <enrike.sanchez@gmail.com>
2021-11-14 16:02:48 -0700

    Added logs to Security Manager

--- src/main/java/mx/tec/web/lab/manager/SecurityManager.java
+++ src/main/java/mx/tec/web/lab/manager/SecurityManager.java
@@ -4,6 +4,8 @@ import java.util.Optional;
 
 import javax.annotation.Resource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
@@ -15,12 +17,15 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Service;
 
+import mx.tec.web.lab.controller.ProductController;
 import mx.tec.web.lab.util.JsonWebTokenUtil;
 import mx.tec.web.lab.vo.CredentialsVO;
 import mx.tec.web.lab.vo.JsonWebTokenVO;
 
 @Service
 public class SecurityManager {
+	private static final Logger log = LoggerFactory.getLogger(SecurityManager.class);	
+	
 	@Resource
 	UserDetailsService userDAO;
 	
@@ -41,10 +46,13 @@ public class SecurityManager {
 		Optional<UsernamePasswordAuthenticationToken> authenticationToken = Optional.empty();
 		UserDetails userDetails = userDAO.loadUserByUsername(username);
 
+		log.info("Authenticating user {} with token", username);
+		
 		if (jwtTokenUtil.validateToken(token, userDetails)) {
 			UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
 					userDetails, null, userDetails.getAuthorities());
 			authenticationToken = Optional.of(usernamePasswordAuthenticationToken);
+			log.info("Token for user {} is valid", username);
 		}
 
 		return authenticationToken;
