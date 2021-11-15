e0e601f4bcb7ff7872bf11bfee1192fd7dfbc2cf
Enrique Sanchez <enrike.sanchez@gmail.com>
2021-11-14 17:47:39 -0700

    Added toString to user and role entities

--- src/main/java/mx/tec/web/lab/entity/Role.java
+++ src/main/java/mx/tec/web/lab/entity/Role.java
@@ -52,4 +52,9 @@ public class Role {
 	public void setUsers(Set<User> users) {
 		this.users = users;
 	}
+
+	@Override
+	public String toString() {
+		return "Role [id=" + id + ", name=" + name + "]";
+	}
 }
--- src/main/java/mx/tec/web/lab/entity/User.java
+++ src/main/java/mx/tec/web/lab/entity/User.java
@@ -82,4 +82,10 @@ public class User {
 	public void setRoles(Set<Role> roles) {
 		this.roles = roles;
 	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", firstname=" + firstname + ", lastname=" + lastname + ", username=" + username
+				+ ", password=" + password + ", roles=" + roles + "]";
+	}
 }
